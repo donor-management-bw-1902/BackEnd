333dda2d0c5a5b5e9102cf714ea0874f98ada89b
BrandCam <BrandCam>
2019-02-04 12:49:17 -0600

    migrations set up

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const { server } = require("./api/server.js");
 
 const port = 3300;
 server.listen(port, () => {
-  console.log(`/n+++ Server Listening On Port ${port} /n`);
+  console.log(`\n+++ Server Listening On Port ${port} +++\n`);
 });
